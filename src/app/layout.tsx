0493125a189bd3a579873ff0cc96f78be62fc559
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2025-02-10 17:16:41 -0400

    install shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter as FontsSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const fontSans = FontsSans({
+  variable: "--font-sans",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
       >
         {children}
       </body>
